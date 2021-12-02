952baffe042ffa9f1e7d4849a8a2efec8ca32ae1
ViniciusU <vini.gama.15@gmail.com>
2021-12-02 11:08:12 -0300

    finalização

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const { v4: uuidv4, validate } = require('uuid');
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,19 +11,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
+  const {username} = request.headers;
+  const user = users.find(user => user.username===username);
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  };
+  
+  request.user = user;
+  return next();
+ 
+
   // Complete aqui
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  
+  const user = request.user;
+  if((user.todos.length < 9)||user.pro){
+    next();
+  }
+
+  if(user.todos.length >= 9 && (!user.pro)){
+    return response.status(403);
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
-}
+  
+  const {username} = request.headers;
+  const {id} = request.params;
+  const user = users.find(user=>user.username === username);
+  const idIsUuid = validate(id);
+  
+  
+  if(!user){
+    return response.status(404);
+  }
+  if(idIsUuid===false){
+    return response.status(400);
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo){
+    return response.status(404).json({ error: 'Mensagem do erro' });
+  };
+    request.todo = todo;
+    request.user = user;
+   
+   return next();
+
+  }
+    
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const {id} = request.params;
+  const user = users.find(user => user.id===id);
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+  request.user = user;
+  return next();
+ 
+ 
 }
 
 app.post('/users', (request, response) => {
